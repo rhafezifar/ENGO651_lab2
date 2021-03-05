546fbc890108e1eacbb40419d74f5d9eec5d5175
rayehe <hafezifar.r@gmail.com>
2021-03-05 00:47:57 -0700

    map added

---
+++ js/calgary-map.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+	var mymap = L.map('mapid').setView([51.0447, -114.0719], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'mapbox/streets-v11',
+	    tileSize: 512,
+	    zoomOffset: -1,
+	    accessToken: 'pk.eyJ1IjoicmF5ZWhlIiwiYSI6ImNrbHZ5NHMyejBkdXcyc214OHlvNmhrZG0ifQ.KXVOh3T-0PdiPnVQ5iMCCQ'
+	}).addTo(mymap);
+
+	var marker = L.marker([51.0447, -114.0719]).addTo(mymap);
+	marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+
+	document.querySelector('button').onclick = function(){
+		const request = new XMLHttpRequest();
+		console.log("Here");
+		let url = `https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '2020-01-21' and issueddate < '2020-01-22'`
+		request.open('GET', url);
+		request.onload = function() {
+			const data = JSON.parse(request.responseText);
+		};
+	};
+});
