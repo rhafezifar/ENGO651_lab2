9c0e64ea4aebe00a7e980c44ace8c70132d1216b
rayehe <hafezifar.r@gmail.com>
2021-03-05 01:53:09 -0700

    date selector for search added

--- js/calgary-map.js
+++ js/calgary-map.js
@@ -1,57 +1,70 @@
-document.addEventListener('DOMContentLoaded', function(){
-	var mymap = L.map('mapid').setView([51.0447, -114.0719], 13);
+document.addEventListener('DOMContentLoaded', function () {
+    $(function () {
+        $("#from-datepicker").datepicker({dateFormat: "yy-mm-dd"});
+    });
+	$( function() {
+		$( "#to-datepicker" ).datepicker({dateFormat: "yy-mm-dd"});
+	} );
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	    maxZoom: 18,
-	    id: 'mapbox/streets-v11',
-	    tileSize: 512,
-	    zoomOffset: -1,
-	    accessToken: 'pk.eyJ1IjoicmF5ZWhlIiwiYSI6ImNrbHZ5NHMyejBkdXcyc214OHlvNmhrZG0ifQ.KXVOh3T-0PdiPnVQ5iMCCQ'
-	}).addTo(mymap);
+    var mymap = L.map('mapid').setView([51.0447, -114.0719], 13);
 
-	var marker = L.marker([51.0447, -114.0719]).addTo(mymap);
-	marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoicmF5ZWhlIiwiYSI6ImNrbHZ5NHMyejBkdXcyc214OHlvNmhrZG0ifQ.KXVOh3T-0PdiPnVQ5iMCCQ'
+    }).addTo(mymap);
 
-	document.querySelector('button').onclick = function(){
-		const request = new XMLHttpRequest();
-		let url = `https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate >= '2020-01-21' and issueddate <= '2020-01-25'`
-		request.open('GET', url);
-		request.onload = function() {
-			const data = JSON.parse(request.responseText);
-			console.log("data loaded");
-			console.log(data);
-			for (let permit of data.features){
-				console.log(permit);
-				console.log(permit.properties.issueddate);
-				console.log(permit.properties.originaladdress);
+    var marker = L.marker([51.0447, -114.0719]).addTo(mymap);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
 
-				const geojsonMarkerOptions = {
-					radius: 6,
-					fillColor: "#8442f5",
-					color: "#000",
-					weight: 1,
-					opacity: 1,
-					fillOpacity: 0.5
-				};
+    document.querySelector('button').onclick = function () {
+        const request = new XMLHttpRequest();
+        const fromdate = document.querySelector('#from-datepicker').value;
+        const todate = document.querySelector('#to-datepicker').value;
+        if (!fromdate || !todate){
+        	alert("From and/or To date is not selected!");
+        	return false;
+		}
+        let url = `https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate >= '${fromdate}' and issueddate <= '${todate}'`
+        request.open('GET', url);
+        request.onload = function () {
+            const data = JSON.parse(request.responseText);
+            console.log("data loaded");
+            console.log(data);
+            for (let permit of data.features) {
+                console.log(permit);
+                console.log(permit.properties.issueddate);
+                console.log(permit.properties.originaladdress);
 
-				const p = permit.properties;
+                const geojsonMarkerOptions = {
+                    radius: 6,
+                    fillColor: "#8442f5",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.5
+                };
 
-				const issuedate = p.issueddate.slice(0,10);
+                const p = permit.properties;
 
-				const popupinfo = `Issue date: ${issuedate}<br>Work Class Group: ${p.workclassgroup}<br>Contractor Name: ${p.contractorname}<br>Community Name: ${p.communityname}<br>Original Address: ${p.originaladdress}`;
+                const issuedate = p.issueddate.slice(0, 10);
 
-				L.geoJSON(permit, {
-					onEachFeature: function (feature, layer) {
-						layer.bindPopup(popupinfo);
-					},
-					pointToLayer: function (feature, latlng) {
-						return L.circleMarker(latlng, geojsonMarkerOptions);
-					}
-				}).addTo(mymap);
+                const popupinfo = `Issue date: ${issuedate}<br>Work Class Group: ${p.workclassgroup}<br>Contractor Name: ${p.contractorname}<br>Community Name: ${p.communityname}<br>Original Address: ${p.originaladdress}`;
 
-			}
-		};
-		request.send();
-	};
+                L.geoJSON(permit, {
+                    onEachFeature: function (feature, layer) {
+                        layer.bindPopup(popupinfo);
+                    },
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, geojsonMarkerOptions);
+                    }
+                }).addTo(mymap);
+
+            }
+        };
+        request.send();
+    };
 });
